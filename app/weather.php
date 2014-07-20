76eaf2f8ff2f09f81554f6cbc4406f176fe8648e
Rahul-Sagore <rahul.sagore@gmail.com>
2014-07-20 19:43:21 +0530

    Minor changes in appearance

--- app/weather.php
+++ app/weather.php
@@ -30,24 +30,24 @@
 				#Calculation for converting temperature from kelvin to celsius
 				$celsius = $data[main][temp] - 273;
 			?>
-<?php echo $data[weather][icon]; ?>
-          <!-- Weather begins -->
-      <div id="weather">      
-        <h2 class="cover-heading">Weather Information at - <?php echo $data[name]; ?> </h2>
+			<?php echo $data[weather][icon]; ?>
+          	<!-- Weather begins -->
+	      <div id="weather">      
+	        <h2 class="cover-heading">Weather Information at - <?php echo $data[name]; ?> </h2>
 
-          <pre><ul>
-            <li>Temperature 		     ---- <?php echo $celsius." Celsius "; ?><img src="http://openweathermap.org/img/w/<?php echo $data[weather][0][icon]; ?>.png"></li>
-            <li>Description                  ---- <?php echo $data[weather][0][description]; ?></li>
-            <li>Sunrise                      ---- <span id="sunrise"><?php echo $data[sys][sunrise]; ?></span></li>
-            <li>Sunrise                      ---- <?php echo gmdate("g:i a", $data[sys][sunrise]	); ?></li>
-            <li>Sunset                       ---- <span id="sunset"><?php echo $data[sys][sunset]; ?> </span></li>
-            <li>Wind                         ---- <?php echo $data[wind][speed]. " mps"; ?></li>
-            <li>Humidity                     ---- <?php echo $data[main][humidity]."%"; ?></li>
-          </ul>  </pre>
-                                       
-      </div>
-      <!-- Weather ends -->
-
+	          <ul>
+	            <li>Temperature 		     ---- <?php echo $celsius." Celsius "; ?><img src="http://openweathermap.org/img/w/<?php echo $data[weather][0][icon]; ?>.png"></li>
+	            <li>Description                  ---- <?php echo $data[weather][0][description]; ?></li>
+	            <li>Sunrise                      ---- <span id="sunrise"><?php echo $data[sys][sunrise]; ?></span></li>
+	            <li>Sunrise                      ---- <?php echo gmdate("g:i a", $data[sys][sunrise]	); ?></li>
+	            <li>Sunset                       ---- <span id="sunset"><?php echo $data[sys][sunset]; ?> </span></li>
+	            <li>Wind                         ---- <?php echo $data[wind][speed]. " mps"; ?></li>
+	            <li>Humidity                     ---- <?php echo $data[main][humidity]."%"; ?></li>
+	          </ul>  
+	                                       
+	      </div>
+	      <!-- Weather ends -->
+	      
           <?php include("includes/footer.inc.php"); ?>
           
         </div>
