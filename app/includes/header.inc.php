406f4e577595d60f50fbb97f09d7331356ac2d53
Rahul-Sagore <rahul.sagore@gmail.com>
2014-07-20 19:42:57 +0530

    changed the homepage link from home.php to index.php

--- app/includes/header.inc.php
+++ app/includes/header.inc.php
@@ -2,7 +2,7 @@
             <div class="inner">
               <h3 class="masthead-brand">Krishi Tirth</h3>
               <ul class="nav masthead-nav">
-                <li class="active"><a href="home.php">Home</a></li>
+                <li class="active"><a href="index.php">Home</a></li>
                 <li><a href="weather.php">Weather</a></li>
                 <li><a href="grain_rates.php">Grain Rates</a></li>
                 <li><a href="contact_us.php">Contact</a></li>
