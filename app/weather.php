38275f1a52acca33b45516f7cb7646fd54f4cad9
Rahul-Sagore <rahul.sagore@gmail.com>
2014-08-01 22:10:30 +0530

    Added Foreach loop to iterate weather forecasting data

--- app/weather.php
+++ app/weather.php
@@ -20,62 +20,84 @@
       <div class="site-wrapper-inner">
 
         <div class="cover-container">
+	
+	    	<!-- including header file -->
+            <?php include("includes/header.inc.php"); ?>
 
-          <?php include("includes/header.inc.php"); ?>
-
-           <!-- Code for fetching Weather Data -->
-	        <?php 
-	        	$json = file_get_contents('http://api.openweathermap.org/data/2.5/weather?q=Indore,in');
+   	    	<!-- Code for fetching Weather Data -->
+            <?php 
+				$json = file_get_contents('http://api.openweathermap.org/data/2.5/forecast?q=Indore,in');
+        	
+        		#Converting weather json data to array
 				$data = json_decode($json, true);
-				#Calculation for converting temperature from kelvin to celsius
-				$celsius = $data[main][temp] - 273;
-			?>
-			<?php echo $data[weather][icon]; ?>
-          	<!-- Weather begins -->
-	      <div id="weather">      
-	        <h2 class="cover-heading">Weather Information at - <?php echo $data[name]; ?> </h2>
+	    	?>
 
-	          <ul>
-	            <li>Temperature 		     ---- <?php echo $celsius." Celsius "; ?><img src="http://openweathermap.org/img/w/<?php echo $data[weather][0][icon]; ?>.png"></li>
-	            <li>Description                  ---- <?php echo $data[weather][0][description]; ?></li>
-	            <li>Sunrise                      ---- <span id="sunrise"><?php echo $data[sys][sunrise]; ?></span></li>
-	            <li>Sunrise                      ---- <?php echo gmdate("g:i a", $data[sys][sunrise]	); ?></li>
-	            <li>Sunset                       ---- <span id="sunset"><?php echo $data[sys][sunset]; ?> </span></li>
-	            <li>Wind                         ---- <?php echo $data[wind][speed]. " mps"; ?></li>
-	            <li>Humidity                     ---- <?php echo $data[main][humidity]."%"; ?></li>
-	          </ul>  
-	                                       
+            <!-- Weather begins -->
+	    	<div id="weather">      
+	        	<h2 class="cover-heading">Weather Information at - <?php echo $data['city']['name']; ?> </h2>
+	          
+				<!-- Table for showing Weather forecasting Data -->
+				<table class="table">
+
+				<thead>
+			          <tr>
+			            <th>Day</th>
+			            <th>Time</th>
+			            <th>Temperature</th>
+			            <th>Description</th>
+			            <th>Wind</th>
+			            <th>Humidity</th>
+			          </tr>
+				</thead>
+
+				<tbody>
+			
+					<?php #For Loop for iterating over weather forecast data
+
+						#Storing weather forecasting data into weather variable
+						    $weather = $data['list'];
+						foreach ($weather as $list){
+					
+						# Date Formatting
+						list($date, $time) = explode(" ", $list['dt_txt']);
+						#Calculation for converting temperature from kelvin to celsius
+						$celsius = $list['main']['temp'] - 273;
+						#Other Weather data initialization
+						$humidity =  $list['main']['humidity'];
+						$description = $list['weather'][0]['description'];
+						$wind = $list['wind']['speed'];
+						$icon = $list['weather'][0]['icon'];
+
+						echo "<tr>
+							<td>$date</td>
+							<td>$time</td>
+							<td>$celsius Celsius <img src='http://openweathermap.org/img/w/$icon.png'></td>
+							<td>$description</td>
+							<td>$wind  mps</td>
+							<td>$humidity %</td>
+							 </tr>";			
+						}
+					 ?>
+				        
+				</tbody>
+
+			   </table>
+
+                        
 	      </div>
-	      <!-- Weather ends -->
+	      <!-- Weather div ends -->
 	      
           <?php include("includes/footer.inc.php"); ?>
           
         </div>
-
-      </div>
-
+		<!-- Cover-container ends -->
+      </div> 
+      <!-- Site-Wrapper-inner ends -->
     </div>
+    <!-- Site-Wrapper ends -->
 
+    <!-- Including javascripts links -->
     <?php include("includes/jsLinks.inc.php"); ?>
-
-    <!-- Script for manipulating weather information -->
-    <script type="text/javascript">
-    	/*var dt = new Date(1405613988 * 1000);
-		var hr =dt.getHours(); 
-		if(hr < 10) hr = '0'+hr;
-		var mn =dt.getMinutes();
-	    if(mn < 10) mn = '0'+mn;
-		var mon =dt.getMonth()+1; 
-		if(mon < 10) mon = '0'+mon;
-		var day =dt.getDate(); 
-		if(day < 10) day = '0'+day;
-		var year =dt.getFullYear(); 
-		$("#date_m").html('get at ' + year+'.'+mon+'.'+day+' '+hr+':'+mn );*/
-
-		var times = SunCalc.getTimes(new Date(), 18.98, 72.83); 
-		$("#sunrise").html(times.sunrise.getHours() + ':' + times.sunrise.getMinutes());
-		$("#sunset").html(times.sunset.getHours() + ':' + times.sunset.getMinutes());
-    </script>
 
   </body>
 </html>
