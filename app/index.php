ee01781ad3303d573958831ae35a22cdee026621
Rahul-Sagore <rahul.sagore@gmail.com>
2014-08-01 22:10:03 +0530

    Renamed home.php file to index.php

---
+++ app/index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Krashi Tirth home page</title>
+
+    <?php include("includes/cssLinks.inc.php"); ?>
+
+  </head>
+
+  <body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <?php include("includes/header.inc.php"); ?>
+
+          <!-- Weather begins -->
+          <div id="weather">      
+            <h1 class="cover-heading">What is Krishi Teerth?</h1>
+            
+              Krishi Teerth is an computerized center, a cabin installed at the campus area of Pilgrimages. This cabin is installed with computer system, internet, LCD T.V screen and two technical staff are appointed. The farmers visiting at Krishi Teerth get new and latest information on agriculture such as improved and certified seeds, fertilizers, agriculture tools and implements, wether fore cast, market report etc.  
+                                           
+          </div>      
+
+          <?php include("includes/footer.inc.php"); ?>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <?php include("includes/jsLinks.inc.php"); ?>
+  </body>
+</html>
