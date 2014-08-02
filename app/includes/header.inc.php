fb5ac9b8616ab8d67b82acce1c16c5e9eb943703
Rahul-Sagore <rahul.sagore@gmail.com>
2014-08-02 18:06:05 +0530

    Added Bootstrap fixed menus

--- app/includes/header.inc.php
+++ app/includes/header.inc.php
@@ -1,11 +1,17 @@
-       <div class="masthead clearfix">
-            <div class="inner">
-              <h3 class="masthead-brand">Krishi Tirth</h3>
-              <ul class="nav masthead-nav">
-                <li class="active"><a href="index.php">Home</a></li>
-                <li><a href="weather.php">Weather</a></li>
-                <li><a href="grain_rates.php">Grain Rates</a></li>
-                <li><a href="contact_us.php">Contact</a></li>
-              </ul>
-            </div>
-          </div>
+	
+		<!-- Fixed navbar -->
+        <div class="navbar navbar-inverse navbar-fixed-top">
+			<div class="navbar-inner">
+			  <div class="container">
+				<a class="brand" href="index.php">Krishi Tirth</a>
+				<ul class="nav">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="weather.php">Weather</a></li>
+					<li><a href="grain_rates.php">Grain Rates</a></li>
+					<li><a href="#">Videos</a></li>
+					<li style="right:0"><a href="contact_us.php">Contact</a></li>
+				</ul>
+			   </div>
+			</div>
+		</div>
+ 
